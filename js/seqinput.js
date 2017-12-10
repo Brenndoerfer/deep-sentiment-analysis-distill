b564aeed6e45ecf2b01c9be38ae6254f03b8f000
Michael Brenndoerfer <michael.brenndoerfer@gmail.com>
2017-12-09 20:04:06 -0800

    rnn

--- js/seqinput.js
+++ js/seqinput.js
@@ -5,9 +5,11 @@ $(document).ready(function () {
     var output = createInputs($(this).val());
     var t = null;
 
+    $('.rnn').empty();
     $('#in-sentence').keydown(function () {
 
         $('.seq-output').empty();
+        $('.rnn').empty();
 
         if (last + 250 < Date.now()) {
             last = Date.now();
@@ -16,20 +18,62 @@ $(document).ready(function () {
             clearTimeout(t);
         }
 
-        if (!$('#in-sentence').val().trim()){
+        if (!$('#in-sentence').val().trim()) {
             clearTimeout(t);
-            console.log($('#in-sentence').val().trim())
+            // console.log($('#in-sentence').val().trim())
             $('.seq-output').empty();
+            $('.rnn').empty();
         }
 
         t = setTimeout(function () {
             $('.seq-output').append(createInputs($('#in-sentence').val()));
             MathJax.Hub.Queue(["Typeset", MathJax.Hub, "seq-output"]);
 
+
+            $('.rnn').append(createCell($('#in-sentence').val()));
+            $('.rnn').append(rnnEnd());
+            MathJax.Hub.Queue(["Typeset", MathJax.Hub, "rnn"]);
+
         }, 250);
 
 
     });
+
+    function createCell(sentence) {
+
+        var cell = '';
+        var word = sentence.trim().split(' ');
+        var opa = 1 / (word.length);
+        for (var i = 0; i < word.length; i++) {
+            cell += ' <div class="block" style="float: left; opacity:' + opa * (i + 1) + '">' +
+                '                    <img src="img/a1.png" width="50" class="a1">' +
+                '                    <div class="wh">$W^H$</div>' +
+                '                    <div class="text-center ht">' +
+                '                        <div    style="padding-top: 18px;">$h_{' + i + '}$</div>' +
+                '                    </div>' +
+                '                    <img src="img/a2.png" width="50" style="float: left; margin-top: -20px">' +
+                '                    <div class="wx">$W^X_{t}$</div>' +
+                '                    <div class="text-center xt">$x_{' + i + '}$</div>' +
+                '                    <div class="text-center out">' + word[i] + '</div>' +
+                '                </div>'
+        }
+        return cell;
+    }
+
+    function rnnEnd() {
+        return ' <div class="block" style="float: left;">' +
+            '                    <img src="img/a1.png" width="50" class="a1">' +
+            '                    <div class="text-center softmax">' +
+            '                        <div style="padding-top: 18px;">$\\sigma$ softmax</div>' +
+            '                    </div>' +
+            '                </div>' +
+            '                <div class="block" style="float: left;">' +
+            '                    <img src="img/a1.png" width="50" class="a1">' +
+            '                    <div class="text-center classification">' +
+            '                        <div style="padding-top: 18px;">$[0.42, 0.58]$</div>' +
+            '                    </div>' +
+            '                </div>';
+    }
 
     function createInputs(sentence) {
 
